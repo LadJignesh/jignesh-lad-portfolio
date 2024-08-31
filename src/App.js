99a70f4ac72991c52350b8db2ec1c036cd6806f0
Jignesh Lad <jlad@beyond.com>
2024-08-31 19:31:37 +0100

    test changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SiteNavbar from './Components/SiteNavbar';
 import Footer from './Components/Footer';
+import Body from './Components/Body';
 
 
 function App() {
   return (
     <div className="App">
       <SiteNavbar />
+      <Body />
       <Footer />
     </div>
   );
---
+++ src/Components/Body.js
@@ -0,0 +1,9 @@
+function Body() {
+    return (
+        <>
+            <p className="text-center mt-4 mb-4">This is change in body</p>
+        </>
+    );
+}
+
+export default Body;
